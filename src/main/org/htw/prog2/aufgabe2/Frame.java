39b4fe0f4fc5f4ee23901440c3fdd5215c828a03
Katrin Lang <langk@htw-berlin.de>
2022-06-20 05:40:29 +0200

    Update Frame.java

--- src/main/org/htw/prog2/aufgabe2/Frame.java
+++ src/main/org/htw/prog2/aufgabe2/Frame.java
@@ -4,9 +4,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Frame {
-    private static final double[][] S_x = new double[][]{new double[]{-1,0,1}, new double[]{-2,0,2}, new double[]{-1,0,1}};
-    private static final double[][] S_y = new double[][]{new double[]{-1,-2,-1}, new double[]{0,0,0}, new double[]{1,2,1}};
-    private double brightness = 1;
 
     private BufferedImage image;
     private BufferedImage tracing;
